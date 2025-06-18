a016b9096530764a5a3eafd4bcb83d72906e44e0
Pham Tung Duong <phamtungduong763@gmail.com>
2025-06-18 09:11:56 +0700

    Update validation rule

--- src/skills/dto/update-skill.dto.ts
+++ src/skills/dto/update-skill.dto.ts
@@ -1,15 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSkillDto } from './create-skill.dto';
-import { IsOptional, IsString, Length } from 'class-validator';
 
-export class UpdateSkillDto extends PartialType(CreateSkillDto) {
-  @IsOptional()
-  @IsString()
-  @Length(1, 255)
-  name?: string;
-
-  @IsOptional()
-  @IsString()
-  @Length(0, 100)
-  category?: string;
-}
+export class UpdateSkillDto extends PartialType(CreateSkillDto) {}
